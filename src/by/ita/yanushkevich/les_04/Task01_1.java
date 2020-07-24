e7a5e4e081c0ee313231331c57a01221af27332b
Victor Y <v6767404@gmail.com>
2020-07-25 01:15:32 +0300

    commit

--- src/by/ita/yanushkevich/les_04/Task01_1.java
+++ src/by/ita/yanushkevich/les_04/Task01_1.java
@@ -1,16 +1,16 @@
 package by.ita.yanushkevich.les_04;
 
-// сфорировать двумерный массив заполненный нулями и вывести на экран
+// сфорировать двумерный массив заполненный 1-цами и вывести на экран
 
 public class Task01_1 {
     public static void main(String[] args) {
 
         int n = 6;
-        int[][] mas = new int[n][n];
+        int[][] mas = new int[n][n]; // по умолчанию все элементы ноыого массива заполнены нулями
 
         for (int i = 0; i < mas.length; i++) {
             for (int j = 0; j < mas[i].length; j++) {
-                mas[i][j] = 0;
+                mas[i][j] = 1;
             }
         }
 
