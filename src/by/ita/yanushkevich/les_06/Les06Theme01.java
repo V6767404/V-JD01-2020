c36b8c768630ddc08d203ebc35c3aeb43e1f2df5
Victor Y <v6767404@gmail.com>
2020-07-29 01:21:17 +0300

    lesson_06_theme

--- src/by/ita/yanushkevich/les_06/Les06Theme01.java
+++ src/by/ita/yanushkevich/les_06/Les06Theme01.java
@@ -7,12 +7,16 @@ public class Les06Theme01 {
         String str1 = "Java";
         String str2 = "Java";
         String str3 = new String ("Java");
+
         System.out.println("-----1");
+
         System.out.println(str1 == str2);
         System.out.println(str1.equals(str2));
         System.out.println(str1 == str3); // сслыки разные
         System.out.println(str1.equals(str3));
+
         System.out.println("-----2");
+
         str3 = str3.intern(); // https://youtu.be/d6WuEw19sHg?list=PLZ49j3s2FXoxpI_SrSMv-ScY2eYpnsQrk&t=1673
         System.out.println(str1 == str3); //ссылки стали ссылаться на один объект т.е. равны
         System.out.println(str1.equals(str3));
@@ -24,9 +28,10 @@ public class Les06Theme01 {
         String str5 = str2 + ""; //создасться новый объект
         System.out.println(str5 == str2); //false
         String str6 = str2.concat(""); //новый объект не создастся // https://youtu.be/d6WuEw19sHg?list=PLZ49j3s2FXoxpI_SrSMv-ScY2eYpnsQrk&t=2084
-
         System.out.println(str6 == str2); //true
+
         System.out.println("-----4");
+
         String str7 = "Java14";
         String str8 = "Java" + "14";
         System.out.println(str7 == str8 ); //true потому что складывались константы
